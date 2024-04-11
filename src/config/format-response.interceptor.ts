d4341fbbdff8e17dde4ab3e48d37bcfe4a723c7c
nindleMac <1104892323@qq.com>
2024-04-11 16:43:27 +0800

    feat: 格式化响应数据

---
+++ src/config/format-response.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Response } from 'express';
+import { map } from 'rxjs/operators';
+
+export interface FormatResponse<T = any> {
+  code: number;
+  data: T;
+  message: string;
+}
+
+@Injectable()
+export class FormatResponseInterceptor<T>
+  implements NestInterceptor<T, FormatResponse>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<FormatResponse<T>> {
+    const response = context.switchToHttp().getResponse<Response>();
+
+    return next.handle().pipe(
+      map((data) => ({
+        code: response.statusCode,
+        message: 'success',
+        data,
+      })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { FormatResponseInterceptor } from './config/format-response.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
