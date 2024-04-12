1c1911a4e45441f88cd8eb7e550bc914ea6cb8d5
nindleMac <1104892323@qq.com>
2024-04-12 22:38:59 +0800

    feat: 证书配置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import * as fs from 'fs';
+import * as https from 'https';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FormatResponseInterceptor } from './config/format-response.interceptor';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./private.key'),
+    cert: fs.readFileSync('./certificate.crt'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.useGlobalInterceptors(new FormatResponseInterceptor());
   await app.listen(6200);
 }
