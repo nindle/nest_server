cca2dc766a60475514a8810aa2092ff61875f3ad
nindleMac <1104892323@qq.com>
2024-04-16 18:05:51 +0800

    test： 代理

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,12 @@ async function bootstrap() {
     cert: fs.readFileSync('./certificate.crt'),
   };
   const app = await NestFactory.create(AppModule, { httpsOptions });
+  app.enableCors({
+    origin: 'https://nindle.me', // 更换为你的前端域名
+    methods: 'GET,POST,PUT,DELETE', // 根据需要配置允许的请求方法
+    // allowedHeaders: 'Content-Type, Authorization', // 根据需要配置允许的头部
+    // credentials: true, // 如果需要支持发送 Cookie，启用此项
+  });
   app.useGlobalInterceptors(new FormatResponseInterceptor());
   await app.listen(6200);
 }
